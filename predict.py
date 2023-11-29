cf8c7f1f100a645aac33f403c5edd20bac8293d5
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-11-29 15:19:17 -0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -32,26 +32,24 @@ def download_online_model(url, dir_name):
     z.extractall(os.path.join(extraction_folder, os.path.basename(zip_name.split(".zip")[0]))
     # print("[+] Model successfully downloaded!")
 
-    """
+    
     # original download script
-    urllib.request.urlretrieve(url, zip_name)
-    print("[~] Extracting zip - {zip_name}")
-    with zipfile.ZipFile(zip_name, "r") as zip_ref:
-        for member in zip_ref.infolist():
-            # skip directories
-            if member.is_dir():
-                continue
-
+    # urllib.request.urlretrieve(url, zip_name)
+    # print("[~] Extracting zip - {zip_name}")
+    # with zipfile.ZipFile(zip_name, "r") as zip_ref:
+    #    for member in zip_ref.infolist():
+    #        # skip directories
+    #        if member.is_dir():
+    #            continue
             # create target directory if it does not exist
-            os.makedirs(extraction_folder, exist_ok=True)
-
+    #        os.makedirs(extraction_folder, exist_ok=True)
             # extract only files directly to extraction_folder
-            with zip_ref.open(member) as source, open(
-                os.path.join(extraction_folder, os.path.basename(member.filename)), "wb"
-            ) as target:
-                shutil.copyfileobj(source, target)
-    print(f"[+] {dir_name} Model successfully downloaded!")
-    """
+    #        with zip_ref.open(member) as source, open(
+    #            os.path.join(extraction_folder, os.path.basename(member.filename)), "wb"
+    #        ) as target:
+    #            shutil.copyfileobj(source, target)
+    # print(f"[+] {dir_name} Model successfully downloaded!")
+
 
 
 class Predictor(BasePredictor):
