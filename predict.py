93f13d801f8917379cb0c38aa147176d8d686c90
PlatformKit <54647524+platform-kit@users.noreply.github.com>
2023-12-05 17:11:50 -0800

    debugging

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-import src.main as m
+import main as m
 import os
 import sys
 import shutil
@@ -14,37 +14,7 @@ sys.path.insert(0, os.path.abspath("src"))
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
-        """Load the model into memory to make running multiple predictions efficient"""
-        from pathlib import Path
-        import requests
-
-        MDX_DOWNLOAD_LINK = 'https://github.com/TRvlvr/model_repo/releases/download/all_public_uvr_models/'
-        RVC_DOWNLOAD_LINK = 'https://huggingface.co/lj1995/VoiceConversionWebUI/resolve/main/'
-
-        BASE_DIR = Path(__file__).resolve().parent.parent
-        mdxnet_models_dir = BASE_DIR / 'mdxnet_models'
-        rvc_models_dir = BASE_DIR / 'rvc_models'
-
-        def dl_model(link, model_name, dir_name):
-            with requests.get(f'{link}{model_name}') as r:
-                r.raise_for_status()
-                with open(dir_name / model_name, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=8192):
-                        f.write(chunk)
-
-        mdx_model_names = ['UVR-MDX-NET-Voc_FT.onnx',
-                           'UVR_MDXNET_KARA_2.onnx', 'Reverb_HQ_By_FoxJoy.onnx']
-        for model in mdx_model_names:
-            print(f'Downloading {model}...')
-            dl_model(MDX_DOWNLOAD_LINK, model, mdxnet_models_dir)
-
-        rvc_model_names = ['hubert_base.pt', 'rmvpe.pt']
-        for model in rvc_model_names:
-            print(f'Downloading {model}...')
-            dl_model(RVC_DOWNLOAD_LINK, model, rvc_models_dir)
-
-        print('All models downloaded!')
-
+        """Load the model into memory to make running multiple predictions efficient"""        
         pass
         
     def predict(
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,4 +2,5 @@
 
 # cog push
 cd realistic-voice-cloning
+bash python3 download.py
 cog push r8.im/platform-kit/rvc-inference
